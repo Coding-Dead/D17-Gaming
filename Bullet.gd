86c0c8a309306b1b39ab0d57b677616cd6285c32
Miłosz Junak <milekjunak@gmail.com>
2024-05-18 15:08:44 +0200

    sql

--- Bullet.gd
+++ Bullet.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 var speed = 1500
 var damage = 10
-var target_dir: Vector2 = Vector2.ZERO
+var target_dir: Vector2
 var time_since_start = 0
-var time_to_live = 1.0
+var time_to_live = 0.8
+var targeted_group: String = "enemy"
+var friendly_group: String = "player"
 
 func _physics_process(delta):
 	if target_dir:
@@ -19,8 +21,14 @@ func set_target(target_direction):
 	target_dir = target_direction
 
 
+func switch_target():
+	targeted_group = "player"
+	friendly_group = "enemy"
+
+
 func _on_body_entered(body):
-	if body.is_in_group("enemy"):
+	if body.is_in_group(targeted_group):
 		body.take_damage(damage)
-	if not body.is_in_group("player"):
+	if not body.is_in_group(friendly_group):
+		print(friendly_group)
 		queue_free()
