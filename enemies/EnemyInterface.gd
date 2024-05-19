6893d446b92769820e8166036b494c2c5439d5f2
Miłosz Junak <milekjunak@gmail.com>
2024-05-19 09:40:13 +0200

    enemies work

--- enemies/EnemyInterface.gd
+++ enemies/EnemyInterface.gd
@@ -27,15 +27,13 @@ func attack_player(): # This has to be overriden in order to work for ranged ene
 
 
 func _physics_process(delta):
-	
 	if player:
 		if in_attack_range:
 			attack_player()
 		else:
-			if touching_monster:
-				position += random_direction * 1.5
-			var direction = (player.position - position).normalized()
-			position += direction * speed * delta
+			var direction = (player.global_position - global_position).normalized()
+			velocity += direction * speed / 20
+			move_and_slide()
 
 func sleep(seconds):
 	await get_tree().create_timer(seconds).timeout 
@@ -47,7 +45,8 @@ func _on_attack_detection_area_body_entered(body):
 
 
 func _on_detection_area_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not player:
+		print("FOUND PLAYER!")
 		player = body
 
 
@@ -55,12 +54,14 @@ func _on_attack_detection_area_body_exited(body):
 	if body.is_in_group("player"):
 		in_attack_range = false
 
+
 func take_damage(_damage : float):
 	curr_health -= _damage
 	if curr_health <= 0:
 		generate(position, 3)
 		queue_free()
-		
+
+
 func generate(position: Vector2, no_items: int):
 	var delta : float = 50.0
 	for i in range(no_items):
