7db2dc1528e561afa80aa85dffb12bf3d8fe488f
norm4nn <szymon.jan.mamon@gmail.com>
2024-05-19 15:11:50 +0200

    Second floor rooms entering

--- floors/elevator_to_second.gd
+++ floors/elevator_to_second.gd
@@ -16,3 +16,23 @@ func _on_body_entered(body):
 		#print(floor_up)
 		body.position= Vector2(-382, -3164)
 	
+
+
+func _on_room_21_body_entered(body):
+	if(body.name=="Player"):
+		body.position= Vector2(-5750, -2700)
+
+
+func _on_area_2d_4_body_entered(body):
+	if(body.name=="Player"):
+		body.position= Vector2(4000, 3300)
+
+
+func _on_area_2d_3_body_entered(body):
+	if(body.name=="Player"):
+		body.position= Vector2(9500, -4900)
+
+
+func _on_area_2d_2_body_entered(body):
+	if(body.name=="Player"):
+		body.position= Vector2(4950, -2550)
